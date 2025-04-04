fcfaf7264e5fabc7c88250e0d40d5e453a220e24
Jean Robertou <j-rob@live.fr>
2025-04-04 18:25:26 +0200

    feat: add cta

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,20 @@
 export const CallToAction = () => {
-  return <div>CallToAction</div>
+  return (
+    <div className="mx-auto w-full max-w-7xl space-y-8 rounded-2xl bg-slate-800 p-4 px-4 pb-0 text-gray-100 lg:py-16">
+      <h2 className="text-center text-4xl font-semibold">
+        Take Your Shooting to the Next Level?
+      </h2>
+
+      <p className="text-center text-lg leading-tight tracking-tight">
+        Join thousands of shooters who trust Recoil to help them improve their
+        shooting.
+      </p>
+
+      <div className="flex items-center justify-center">
+        <button className="rounded-xl bg-gray-100 px-6 py-4 text-center text-lg font-semibold leading-tight tracking-tight text-gray-900 transition-colors duration-500 hover:bg-slate-700 hover:text-gray-100">
+          Start tracking your progress today!
+        </button>
+      </div>
+    </div>
+  )
 }
---
+++ src/components/WhyRecoil.tsx
@@ -0,0 +1,36 @@
+import { Container } from './Container'
+
+export const WhyRecoil = () => {
+  return (
+    <div className="space-y-8 rounded-2xl bg-slate-800 p-4 pb-0 text-gray-100 lg:py-8">
+      <Container>
+        <h2 className="text-center text-4xl font-semibold">
+          For Shooters Who Want More
+        </h2>
+
+        <div className="mt-12">
+          <p>
+            <span className="font-semibold">
+              Every great marksman relies on more than just instinct,
+            </span>{' '}
+            they rely on data. Recoil is built for shooters who want to refine
+            their accuracy, optimize their gear, and push their limits with real
+            performance tracking.
+          </p>
+          <p>
+            <span className="font-semibold">No more wasted rounds.</span> See
+            your improvement over time, understand what works, and train with
+            purpose. Whether you're dialing in a new optic, testing ammo, or
+            chasing tighter groups, Recoil helps you shoot with precision.
+          </p>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+/* 
+Every great marksman relies on more than just instinct—they rely on data. Recoil is built for shooters who want to refine their accuracy, optimize their gear, and push their limits with real performance tracking.
+
+No more wasted rounds. See your improvement over time, understand what works, and train with purpose. Whether you're dialing in a new optic, testing ammo, or chasing tighter groups, Recoil helps you shoot with precision.
+ */
